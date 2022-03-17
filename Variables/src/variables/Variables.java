55af6ec89e02a7583828e5bd318ab9cbbbb92ac8
diego40g <diegopaznaula@hotmail.com>
2022-03-16 23:55:34 -0500

    Tipos de datos Int y String

---
+++ Variables/src/variables/Variables.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variables;
+
+/**
+ *
+ * @author User-PC
+ */
+public class Variables {
+
+    public static void main(String[] args) {
+        //Definimos la varaible
+        int miVariableEntera=10;
+        System.out.println(miVariableEntera);
+        //Modifcando el valor de la varaible
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+        
+        String miVaraibleCadena="Saludos";
+        System.out.println(miVaraibleCadena);
+        miVaraibleCadena="Adios";
+        System.out.println(miVaraibleCadena);
+    }
+    
+}
